b61f11608c92b1348e533a341026fdabba280f1e
sneakykestrel <141267380+sneakykestrel@users.noreply.github.com>
2024-09-22 00:37:32 +0100

    Add project files.

---
+++ IAYB_UncapFramerate/Mod.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.Mono;
+using HarmonyLib;
+using UnityEngine;
+
+namespace IAYB_UncapFramerate
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginversion)]
+    public class Mod : BaseUnityPlugin
+    {
+        public const string pluginGuid = "kestrel.iamyourbeast.uncapframerate";
+        public const string pluginName = "Uncap Framerate";
+        public const string pluginversion = "1.0.0.0";
+
+        public static ConfigEntry<float> minFps;
+        public static ConfigEntry<float> maxFps;
+
+        public void Awake() {
+
+            minFps = Config.Bind("Options", "Minimum Framerate", 10f, "The minimum framerate shown on the slider (minimum value: 1)");
+            maxFps = Config.Bind("Options", "Maximum Framerate", 240f, "The maximum framerate shown on the slider");
+
+            Logger.LogInfo("Hiiiiiiiiiiii :3");
+            Harmony harmony = new Harmony(pluginGuid);
+            harmony.PatchAll();
+        }
+    }
+
+    //Make SaveSettings use the raw value from sliderFramerate rather than adjusting for the weird slider scale
+    [HarmonyPatch(typeof(UISettingsSubMenuVisual), nameof(UISettingsSubMenuVisual.SaveSettings))]
+    public class PatchVisualSave
+    {
+        [HarmonyPostfix]
+        public static void Postfix(UISettingsOptionSlider ___sliderFramerate) {
+            GameManager.instance.saveManager.CurrentSettings.FramerateCap = Mathf.RoundToInt(___sliderFramerate.GetValue());
+        }
+    }
+
+    //Make RevertSettings use the raw value from sliderFramerate rather than adjusting for the weird slider scale
+    [HarmonyPatch(typeof(UISettingsSubMenuVisual), nameof(UISettingsSubMenuVisual.RevertSettings))]
+    public class PatchVisualRevert
+    {
+        [HarmonyPostfix]
+        public static void Postfix(UISettingsOptionSlider ___sliderFramerate) {
+            ___sliderFramerate.SetValue(GameManager.instance.saveManager.CurrentSettings.FramerateCap);
+        }
+    }
+
+
+
+    //On start, set value of slider to -1 (special value to indicate slider needs to be adjusted)
+    [HarmonyPatch(typeof(UISettingsSubMenuVisual), "Start")]
+    public class PatchVisualStart
+    {
+        [HarmonyPostfix]
+        public static void Postfix(UISettingsOptionSlider ___sliderFramerate) {
+            ___sliderFramerate.SetValue(-1);
+            ___sliderFramerate.SetValue(GameManager.instance.saveManager.CurrentSettings.FramerateCap); //clean up after ourselves
+        }
+    }
+
+    //Adjust slider if -1 is passed to SetValue
+    [HarmonyPatch(typeof(UISettingsOptionSlider), nameof(UISettingsOptionSlider.SetValue))]
+    public class PatchSlider
+    {
+        [HarmonyPrefix]
+        public static void Prefix(float value, ref UnityEngine.UI.Slider ___slider, ref float ___valueMultiplier) {
+
+            if (value == -1) {
+                ___valueMultiplier = 1;
+                ___slider.minValue = Mathf.Clamp(Mod.minFps.Value, 1, Mod.maxFps.Value);
+                ___slider.maxValue = Mathf.Max(Mod.maxFps.Value, Mod.minFps.Value + 1);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Uncap Framerate mod for I Am Your Beast
+
+This mod:
+
+- removes the 120 fps framerate cap that for some reason exists in the game
+- allows the framerate cap slider to be set to any whole number rather than multiples of 30
+- allows you to customise the bounds of the framerate slider
+
+have fun :3
